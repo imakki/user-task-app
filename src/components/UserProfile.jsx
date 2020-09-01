52e75bbd289bc769e2fe6ad859ca51315881f34c
Ankit <ankityc143>
2020-09-01 23:56:37 +0530

    initial dependencies and structure setup

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const UserProfile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default UserProfile;
